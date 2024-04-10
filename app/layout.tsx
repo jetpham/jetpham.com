30ef9bacb5abe6a39343e25c175d90b0d91cde1e
jet pham <55770902+jetpham@users.noreply.github.com>
2024-04-09 23:15:21 -0700

    fixed layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ import Head from "next/head";
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
   title: {
-    default: "Home",
+    default: "Main",
     template: "%s | Jet Pham",
   },
   description: "This is my portfolio.",
@@ -45,16 +45,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html>
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      lang="en" className=
-      {cx(
+    <html
+      lang="en"
+      className={cx(
         "text-black bg-white dark:text-white dark:bg-black",
         GeistSans.variable,
         GeistMono.variable
       )}
+    >
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
           <Navbar />
