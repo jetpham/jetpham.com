892245a99fce72b97e291aa640d0e7bd67c94cfe
jet pham <55770902+jetpham@users.noreply.github.com>
2024-04-09 22:58:30 -0700

    fixed silly error

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,7 @@ import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import Footer from "./components/footer";
 import { baseUrl } from "./sitemap";
-import Head from 'next/head'
-
+import Head from "next/head";
 
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
@@ -50,13 +49,12 @@ export default function RootLayout({
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      lang="en"
-      className={cx(
+      lang="en" className=
+      {cx(
         "text-black bg-white dark:text-white dark:bg-black",
         GeistSans.variable,
         GeistMono.variable
       )}
-    >
       <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
           <Navbar />
